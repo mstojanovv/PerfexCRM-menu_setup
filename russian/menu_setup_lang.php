8e845f9c2aa3c1403bfe212574711086e9d1d7eb
Kirill Nikitin <Kiriniy@users.noreply.github.com>
2020-01-15 01:52:58 +0300

    Update language
    Lexical corrections.

--- russian/menu_setup_lang.php
+++ russian/menu_setup_lang.php
@@ -3,7 +3,7 @@
 # Version 2.3.0
 $lang['menu_builder']        = 'Настройка меню';
 $lang['main_menu']           = 'Главное меню';
-$lang['setup_menu']          = 'Настройка меню';
+$lang['setup_menu']          = 'Меню настроек';
 $lang['utilities_menu_icon'] = 'Значок';
-$lang['active_menu_items']   = 'Элементы активного меню';
+$lang['active_menu_items']   = 'Активные пункты меню';
 $lang['utilities_menu_save'] = 'Сохранить меню';
