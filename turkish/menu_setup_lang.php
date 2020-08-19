cda8996484a722de43d50a0d560079954e436919
Seyit Acar <14896676+seyitacar@users.noreply.github.com>
2020-08-19 10:29:16 +0300

    Update menu_setup_lang.php
    Translation corrections completed.

--- turkish/menu_setup_lang.php
+++ turkish/menu_setup_lang.php
@@ -1,9 +1,9 @@
 <?php
 
 # Version 2.3.0
-$lang['menu_builder']        = 'Menu Kurulumu';
-$lang['main_menu']           = 'Ana Menu';
-$lang['setup_menu']          = 'Menu Kurulumu';
-$lang['utilities_menu_icon'] = 'Icon';
-$lang['active_menu_items']   = 'Aktif Menu Nesneleri';
+$lang['menu_builder']        = 'Menü Kurulumu';
+$lang['main_menu']           = 'Ana Menü';
+$lang['setup_menu']          = 'Ayarlar Menüsü';
+$lang['utilities_menu_icon'] = 'Simge';
+$lang['active_menu_items']   = 'Aktif Menü Öğeleri';
 $lang['utilities_menu_save'] = 'Menüyü Kaydet';
